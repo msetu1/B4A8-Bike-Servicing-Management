0006e53381c7c1095026f3b990a0325c5308f013
msetu1 <msetu5763@gmail.com>
2025-05-06 23:32:22 +0600

    catch async and pick functionality done

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import status from "http-status-codes";
 
 const app: Application = express();
 app.use(cors());
@@ -22,5 +23,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Bike Servicing Management Server is running");
 });
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(status.NOT_FOUND).json({
+      success: false,
+      message: "API NOT FOUND",
+      error: {
+        path: req.originalUrl,
+        message: "Your requested path is not found",
+      },
+    });
+  });
 
 export default app;
